c6d352ec108b3b2e1b1cf4d701f5ccab6f0e5e98
Josue Zarzosa <josketres@gmail.com>
2017-02-25 08:58:54 +0100

    First working version of lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,45 @@
+package lexer
+
+import "github.com/josketres/monkey/token"
+
+type Lexer struct {
+    input        string
+    position     int    // current position in input (points to current char)
+    readPosition int    // current reading position in input (after current char)
+    ch           byte   // current char under examination
+}
+
+func New(input string) *Lexer {
+    l := &Lexer{input : input}
+    l.readChar()
+    return l
+}
+
+func (l *Lexer) readChar() {
+    if l.readPosition >= len(l.input) {
+        l.ch = 0
+    } else {
+        l.ch = l.input[l.readPosition]
+    }
+    l.position = l.readPosition
+    l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+    var tok token.Token
+
+    switch l.ch {
+    case '=':
+        tok = newToken(token.ASSIGN, l.ch)
+    case 0:
+        tok.Literal = ""
+        tok.Type = token.EOF
+    }
+
+    l.readChar()
+    return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+    return token.Token{Type: tokenType, Literal: string(ch)}
+}
