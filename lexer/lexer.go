8fe938832431ab8c0b7c2b23759f637513434a0a
Josue Zarzosa <josketres@gmail.com>
2017-02-25 09:28:25 +0100

    Add all operators and single char tokens.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -31,6 +31,20 @@ func (l *Lexer) NextToken() token.Token {
     switch l.ch {
     case '=':
         tok = newToken(token.ASSIGN, l.ch)
+    case '+':
+        tok = newToken(token.PLUS, l.ch)
+    case ',':
+        tok = newToken(token.COMMA, l.ch)
+    case ';':
+        tok = newToken(token.SEMICOLON, l.ch)
+    case '(':
+        tok = newToken(token.LPAREN, l.ch)
+    case ')':
+        tok = newToken(token.RPAREN, l.ch)
+    case '{':
+        tok = newToken(token.LBRACE, l.ch)
+    case '}':
+        tok = newToken(token.RBRACE, l.ch)
     case 0:
         tok.Literal = ""
         tok.Type = token.EOF
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,13 +6,20 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-    input := "="
+    input := "=+,;(){}"
 
     tests := []struct {
         expectedType token.TokenType
         expectedLiteral string
     } {
         {token.ASSIGN, "="},
+        {token.PLUS, "+"},
+        {token.COMMA, ","},
+        {token.SEMICOLON, ";"},
+        {token.LPAREN, "("},
+        {token.RPAREN, ")"},
+        {token.LBRACE, "{"},
+        {token.RBRACE, "}"},
         {token.EOF, ""},
     }
 
