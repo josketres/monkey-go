15bfb9572d4dd4651703a28370c8eaa26321c932
Josue Zarzosa <josketres@gmail.com>
2017-02-26 14:15:02 +0100

    Partialy parse let statements.

--- parser/parser.go
+++ parser/parser.go
@@ -28,6 +28,56 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-    return &ast.Program{}
+    program := &ast.Program{}
+    program.Statements = []ast.Statement{}
+
+    for p.curToken.Type != token.EOF {
+        stmt := p.parseStatement()
+        if stmt != nil {
+            program.Statements = append(program.Statements, stmt)
+        }
+        p.nextToken()
+    }
+
+    return program
 }
 
+func (p *Parser) parseStatement() ast.Statement {
+    switch p.curToken.Type {
+    case token.LET:
+        return p.parseLetStatement()
+    default:
+        return nil
+    }
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+    stmt := &ast.LetStatement{Token : p.curToken}
+
+    if !p.expectPeek(token.IDENT) {
+        return nil
+    }
+
+    stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+    if !p.expectPeek(token.ASSIGN) {
+        return nil
+    }
+    
+    // mvp implementation to satisfy our tests
+    // keep skipping until semicolon
+    for p.curToken.Type != token.SEMICOLON {
+        p.nextToken()
+    }
+
+    return stmt
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+    if p.peekToken.Type == t {
+        p.nextToken()
+        return true
+    } else {
+        return false
+    }
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,7 +2,7 @@ package parser
 
 import (
     "testing"
-//  "github.com/josketres/monkey-go/ast"
+    "github.com/josketres/monkey-go/ast"
     "github.com/josketres/monkey-go/lexer"
 )
 
@@ -20,5 +20,47 @@ func TestLetStatements(t *testing.T) {
     if program == nil {
         t.Fatalf("ParseProgram() returned nil")
     }
+    if length := len(program.Statements); length != 3 {
+        t.Fatalf("program.Statements does not contain 3 statements. got=%d", length)
+    }
 
+    tests := []struct {
+        expectedIdentifier string
+    }{
+        {"x"},
+        {"y"},
+        {"foobar"},
+    }
+
+    for i, tt := range tests {
+        stmt := program.Statements[i]
+        if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+            return
+        }
+    }
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, expectedIdentifier string) bool {
+    if tl := s.TokenLiteral(); tl != "let" {
+        t.Errorf("s.TokenLiteral() not 'let'. got=%q", tl)
+        return false
+    }
+
+    letStmt, ok := s.(*ast.LetStatement)
+    if !ok {
+        t.Errorf("s not *ast.LetStatement. got=%T", s)
+        return false
+    }
+
+    if n := letStmt.Name.Value; n != expectedIdentifier {
+        t.Errorf("letStmt.Name.Value not '%s'. got=%s", expectedIdentifier, n)
+        return false
+    }
+
+    if tl := letStmt.Name.TokenLiteral(); tl != expectedIdentifier {
+        t.Errorf("s.Name not '%s'. got=%s", expectedIdentifier, tl)
+        return false
+    }
+
+    return true
 }
